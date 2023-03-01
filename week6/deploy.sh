fdead58e4817184eb56683e07aaf116b29a81a69
Yen Jung Chen <jacky010080@gmail.com>
2023-03-01 16:11:05 +0800

    更改sh deploy.sh

--- week6/deploy.sh
+++ week6/deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://jacky010080.github.io
-# git push -f git@github.com:jacky010080/jacky010080.github.io.git master
+git push -f git@github.com:jacky010080/jacky010080.github.io.git master
 
 # 部署到 https://jacky010080.github.io/hexschool_Vue3_main/week6/
-git push -f git@github.com:jacky010080/hexschool_Vue3_main/week6/.git master:gh-pages
+# git push -f git@github.com:jacky010080/hexschool_Vue3_main/week6/.git master:gh-pages
 
 cd -
